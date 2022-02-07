20c18c4221e5b2f29d64b06f37a5140e32e46655
Jacob N <jrnels10@gmail.com>
2022-02-06 20:04:14 -0700

    no production.yaml

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { Transport } from '@nestjs/microservices';
 import { UserModule } from './user.module';
-import * as config from 'config';
 import { AppModule } from './app/app.module';
 
 async function bootstrap() {
-  const serverConfig = config.get('server');
   const app = await NestFactory.create(AppModule);
   app.enableCors({ origin: '*' });
   app.connectMicroservice({
     transport: Transport.TCP,
   });
   await app.startAllMicroservices();
-  const port = process.env.PORT || serverConfig.port;
+  const port = process.env.PORT || 5001;
 
   await app.listen(port, () => console.log(`Listening on port ${port}`));
 }
