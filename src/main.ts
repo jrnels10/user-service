ce90c8b9c5cda58224c0455af246ad016679e18d
Jacob N <jrnels10@gmail.com>
2022-02-06 19:28:09 -0700

    heroku deploy

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { Transport } from '@nestjs/microservices';
 import { UserModule } from './user.module';
+import * as config from 'config';
 
 async function bootstrap() {
+  const serverConfig = config.get('server');
   const app = await NestFactory.create(UserModule);
+  app.enableCors({ origin: '*' });
   app.connectMicroservice({
     transport: Transport.TCP,
   });
   await app.startAllMicroservices();
-  await app.listen(3001, () => console.log(`Listening on port ${3001}`));
+  const port = process.env.PORT || serverConfig.port;
+  await app.listen(port, () => console.log(`Listening on port ${port}`));
 }
 bootstrap();
